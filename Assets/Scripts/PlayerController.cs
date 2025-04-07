68d64935090537aa90b8d767b8abf74d97ef1186
Mau <mauvepinktree@gmail.com>
2025-04-07 12:51:07 -0300

    add physics based movement example

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,23 @@ public class PlayerController : MonoBehaviour
     private Vector3 velocity = new Vector3();
     [SerializeField]
     private float speed = 10.0f;
+    [SerializeField]
+    private Rigidbody rb;
     public void UpdateVelocity(Vector2 velocity)
     {
-        this.velocity = (Vector3)(velocity * (speed + Time.deltaTime));
+        this.velocity.x = velocity.x * (speed + Time.deltaTime);
+        this.velocity.z = velocity.y * (speed + Time.deltaTime);
     }
 
     private void MovePlayer()
     {
         this.transform.position += (Vector3)(velocity * Time.deltaTime);
+    }
+
+    private void MyFixedUpdate()
+    {
+        //rb.AddForce(new Vector3(velocity.x, 0, velocity.y) * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+        //rb.AddForce(new Vector3(velocity.x, 0, velocity.y) * speed, ForceMode.Force);
     }
 
     private void Update()
